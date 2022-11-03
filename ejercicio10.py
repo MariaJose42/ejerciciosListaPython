f1ff9a492a3b034b55bda3da6fd32eac10acabeb
MariaJose42 <113972767+MariaJose42@users.noreply.github.com>
2022-11-03 17:08:31 +0100

    Update ejercicio10.py

--- ejercicio10.py
+++ ejercicio10.py
@@ -2,3 +2,18 @@
 '''
 Crear un programa que cree un matriz de 5 x 5 con números aleatorios
 '''
+
+import random
+
+def crear_fila():
+    fila=[]
+    for i in range(0,5):
+        fila.append(random.randint(0,10))
+    return fila
+
+def crear_matriz():
+    matriz=[]
+    while(len(matriz)<5):
+        matriz.append(crear_fila())
+    for i in matriz:
+        print (i)
